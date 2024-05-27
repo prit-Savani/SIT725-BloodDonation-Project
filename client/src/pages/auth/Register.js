559b06f8ed80c57c0992093b2bf3edc93476f6b5
veric01 <163263646+veric01@users.noreply.github.com>
2024-05-28 02:33:22 +1000

    Added user register component Register.js

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -4,18 +4,24 @@ import { useSelector } from "react-redux";
 import Spinner from "../../components/shared/Spinner";
 
 const Register = () => {
+  // Extract loading and error state from the auth slice of Redux store
   const { loading, error } = useSelector((state) => state.auth);
+
   return (
     <>
+      {/* Display error message if error state is not null */}
       {error && <span>{alert(error)}</span>}
+      {/* Display spinner if loading state is true, otherwise display registration form */}
       {loading ? (
         <Spinner />
       ) : (
         <div className="row g-0">
           <div className="col-md-8 form-banner ">
+            {/* Display banner image */}
             <img src="./assets/images/banner2.jpg" alt="registerImage" />
           </div>
           <div className="col-md-4 form-container">
+            {/* Render registration form */}
             <Form
               formTitle={"Register"}
               submitBtn={"Register"}
